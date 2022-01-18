7a5cb712c109e6f11d18c3693bea9877ab4a6c0b
Spencer Johnson <sljohnson2222@gmail.com>
2022-01-18 09:01:00 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return (a + b);
+}
+function subtract(a, b) {
+    return (a - b);
+}
+function multiply(a, b) {
+    return (a * b);
+}
+function divide(a, b) {
+    return (a / b);
+}
+//_____________
+
+n = n;
+function increment(n) {
+    return (n += 1);
+}
+increment(n);
+
+n = n;
+function decrement(n) {
+    return (n -= 1);
+}
+increment(n);
+//______________
+
+
+function makeInt(string) {
+    return (parseInt(string, 10));
+}
+
+
+function preserveDecimal(string) {
+    return(parseFloat(string, 10));
+}
